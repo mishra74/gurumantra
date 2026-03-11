0a7abb1496794b1a3f8d73c43870146db787389a
Niraj <98806576+mishra74@users.noreply.github.com>
2026-03-11 16:41:58 +0530

    upadte recording

--- app/Http/Controllers/ZoomWebhookController.php
+++ app/Http/Controllers/ZoomWebhookController.php
@@ -4,39 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ZoomRecording;
+    use Carbon\Carbon;
+
 
 class ZoomWebhookController extends Controller
 {
 
 public function handle(Request $request)
 {
+    $data = $request->all();
 
-$data = $request->all();
+    if(isset($data['event']) && $data['event'] == 'recording.completed')
+    {
+        $meeting = $data['payload']['object'];
 
-if($data['event'] == 'recording.completed')
-{
+        foreach($meeting['recording_files'] as $file)
+        {
+            ZoomRecording::create([
+                'meeting_id' => $meeting['id'],
+                'topic' => $meeting['topic'] ?? null,
+                'recording_url' => $file['play_url'] ?? null,
+                'download_url' => $file['download_url'] ?? null,
+                'recording_start' => Carbon::parse($file['recording_start'])->format('Y-m-d H:i:s'),
+                'recording_end' => Carbon::parse($file['recording_end'])->format('Y-m-d H:i:s')
+            ]);
+        }
+    }
 
-$meeting = $data['payload']['object'];
-
-foreach($meeting['recording_files'] as $file)
-{
-
-ZoomRecording::create([
-
-'meeting_id' => $meeting['id'],
-'topic' => $meeting['topic'],
-'recording_url' => $file['play_url'],
-'download_url' => $file['download_url'],
-'recording_start' => $meeting['recording_start'],
-'recording_end' => $meeting['recording_end']
-
-]);
-
-}
-
-}
-
-return response()->json(['status'=>'success']);
-
+    return response()->json(['status'=>'success']);
 }
 }
